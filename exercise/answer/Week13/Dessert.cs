a20c08f11091b65bd90d61a80e612aef55dac9bd
fordevoted <210509fssh@gmail.com>
2021-05-25 12:41:01 +0800

    update exercise week13 answer

---
+++ exercise/answer/Week13/Dessert.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Dessert : Food
+    {
+        public Dessert(string name, int price, int number)
+        {
+            setName(name);
+            setPrice(price);
+            setNumber(number);
+            setAlreadyApplyDiscount(false);
+        }
+
+
+        public override void order(ref Food[] orderlist, ref int orderListPointer)
+        {
+            // third beverage would be 30% off  
+            int[] check = {-1, -1};
+            int count = 0;
+            for (int i = 0; i < orderListPointer; i++)
+            {
+                if (orderlist[i] is Dessert && !orderlist[i].getAlreadyApplyDiscount())
+                {
+                    check[count] = i;
+                    count++;
+                }
+
+                if (count >= 2)
+                {
+                    break;
+                }
+            }
+            if (! check.Contains(-1))
+            {
+                orderlist[check[0]].setAlreadyApplyDiscount(true);
+                orderlist[check[1]].setAlreadyApplyDiscount(true);
+               
+                orderlist[orderListPointer] = new Dessert(name: getName(), price: getPrice(), number: getNumber());
+                orderlist[orderListPointer].setAlreadyApplyDiscount(true);
+                orderListPointer += 1;
+                
+                orderlist[orderListPointer] = new Dessert(name: getName(), price: 0, number: getNumber());
+                orderlist[orderListPointer].setAlreadyApplyDiscount(true);
+                
+            }
+            else
+            {
+                orderlist[orderListPointer] = new Dessert(name: getName(), price: getPrice(), number: getNumber());
+            }
+            orderListPointer += 1;
+        }
+    }
+}
---
+++ exercise/answer/Week13/Meal.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp1
+{
+    public class Meal : Food
+    {
+        public Meal(string name, int price, int number)
+        {
+            setName(name);
+            setPrice(price);
+            setNumber(number);
+            setAlreadyApplyDiscount(false);
+        }
+
+
+        public override void order(ref Food[] orderlist, ref  int orderListPointer)
+        {
+            // third beverage would be 30% off  
+            int[] check = {-1};
+            for (int i = 0; i < orderListPointer; i++)
+            {
+                if (orderlist[i] is Meal && !orderlist[i].getAlreadyApplyDiscount())
+                {
+                    check[0] = i;
+                    break;
+                }
+
+                
+            }
+            if (check[0]!= -1)
+            {
+                orderlist[check[0]].setAlreadyApplyDiscount(true);
+                orderlist[orderListPointer] = new Meal(name: getName(), price: (int)(getPrice() * 0.85), number: getNumber());
+                orderlist[orderListPointer].setAlreadyApplyDiscount(true);
+               
+            }
+            else
+            {
+                orderlist[orderListPointer] = new Meal(name: getName(), price: getPrice(), number: getNumber());
+
+            }
+            orderListPointer += 1;
+        }
+    }
+}
