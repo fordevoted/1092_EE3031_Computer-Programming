9e40b391cae86d97c99504d6de0671816c7c3b40
giginepuran <61973060+giginepuran@users.noreply.github.com>
2021-06-15 14:23:50 +0800

    Add files via upload

---
+++ exercise/answer/Week16/MakeMusic.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+namespace week16
+{
+    public class MakeMusic
+    {
+        // Medium
+        public static void Wav(string path, string toneFile, string durationFile, string outputFilename, int type = 0)
+        {
+            bool toneFileExist = File.Exists(path+toneFile);
+            bool durationFileExist = File.Exists(path+durationFile);
+            bool pathExist = Directory.Exists(path);
+            bool validToneFormat = (Path.GetExtension(path+toneFile) == ".txt");
+            bool validDurationFormat = (Path.GetExtension(path+durationFile) == ".txt");
+            bool validOutputFormat = (Path.GetExtension(outputFilename) == ".wav");
+            try
+            {
+                if (!(validToneFormat && validDurationFormat))
+                {//Duration and tone are not .txt format.
+                    throw new InputFileFormatException(String.Format("{0}{1}", validToneFormat?"":$"{toneFile}, ", validDurationFormat?"":durationFile));
+                }
+                else if (!pathExist)
+                {//Directory is not exist.
+                    throw new DirectoryNotFoundException(path);
+                }
+                else if (!(toneFileExist && durationFileExist))
+                {//File not found.
+                    throw new FileNotFoundException(String.Format("{0}{1}", toneFileExist?"":$"{toneFile}, ", durationFileExist?"":durationFile));
+                }
+                else if (!validOutputFormat)
+                {//Output file is not .wav format.
+                    throw new OutputFileFormatException(outputFilename);
+                }
+                WavGenerator hnd = new WavGenerator(path);
+                hnd.Read(toneFile, durationFile);
+                hnd.Write(outputFilename, type);
+            }
+            catch (FileNotFoundException e)
+            {
+                e.Print();
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                e.Print();
+            }
+            catch (InputFileFormatException e)
+            {
+                e.Print();
+            }
+            catch (OutputFileFormatException e)
+            {
+                e.Print();
+            }
+        }
+        public class FileNotFoundException : Exception
+        {
+            private string filename;
+            public FileNotFoundException(string whichFile)
+            {
+                this.filename = whichFile;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"File Not found. ({filename})");
+            }
+        }
+        public class DirectoryNotFoundException : Exception
+        {
+            private string path;
+            public DirectoryNotFoundException(string path)
+            {
+                this.path = path;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Directory is not exist. (path)");
+            }
+        }
+        public class InputFileFormatException : Exception
+        {
+            private string filename;
+            public InputFileFormatException(string whichFile)
+            {
+                this.filename = whichFile;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Input files must be .txt files. ({filename})");
+            }
+        }
+        public class OutputFileFormatException : Exception
+        {
+            private string filename;
+            public OutputFileFormatException(string whichFile)
+            {
+                this.filename = whichFile;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Output file must be .wav format. ({filename})");
+            }
+        }
+    }
+}
---
+++ exercise/answer/Week16/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+
+namespace week16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Easy();
+            //Medium();
+            //Hard_ModulateThePitch();
+            Hard_ModulateTheBPM();
+        }
+        public static void Easy()
+        {
+            string path = @"C:\Users\TA\workshop\week16\sample\Megalovania\";
+            string tonesFile = "tone.txt";
+            string durationsFile = "duration.txt";
+            string[] tones = File.ReadAllText($"{path}{tonesFile}")
+                .Replace("\n", "").Replace("\r", "").Split(' ');
+            string[] durations = File.ReadAllText($"{path}{durationsFile}")
+                .Replace("\n", "").Replace("\r", "").Split(' ');
+            int len = tones.Length;
+            Console.WriteLine("Megalovania");
+            Console.WriteLine("tone | duration");
+            for (int i = 0; i < len; i++)
+                Console.WriteLine(" {0,-3} | {1,-4}", tones[i], durations[i]);
+            Console.ReadLine();
+        }
+        public static void Medium()
+        {
+            // Case: File Not found
+            MakeMusic.Wav(@"C:\Users\TA\workshop\week16\sample\Dejavu\", "GodIsAGirl.txt", "DamDadiDo.txt", "out0.wav");
+
+            // Case: Path doesn;t exist
+            MakeMusic.Wav(@"C:\Users\TA\workshop\week9999\sample\Sample\Wily'sCastle\", "tone.txt", "duration.txt", "out1.wav", 1);
+
+            // Case: Duration and tone are not .txt format.
+            MakeMusic.Wav(@"C:\Users\TA\workshop\week16\sample\Megalovania\", "tone.dat", "duration.jpg", "out2.wav", 2);
+            
+            // Case: Output music file are not .wav format.
+            MakeMusic.Wav(@"C:\Users\TA\workshop\week16\sample\MagicForest\", "tone.txt", "duration.txt", "out3.mp3", 3);
+            Console.ReadLine();
+        }
+        public static void Hard_ModulateThePitch()
+        {
+            string tones = "";
+            using (StreamReader sr = new StreamReader(@"C:\Users\TA\workshop\week16\sample\Megalovania\tone.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] pitchs = line.Split(" ");
+                    for (int i = 0; i < pitchs.Length; i++)
+                    {
+                        if (pitchs[i]=="") 
+                        {
+                            tones += "\n";
+                        }
+                        else
+                        {
+                            string pitch = (pitchs[i])[pitchs[i].Length-1]=='b'?pitchs[i].Replace("b",""):$"{pitchs[i]}#";
+                            tones += $"{pitch} ";
+                        }
+                    }
+                }
+                tones = tones.Remove(tones.Length-1);
+            }
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\TA\workshop\week16\sample\Megalovania\newTone.txt"))
+            {
+                sw.Write(tones);
+            }
+            MakeMusic.Wav(@"C:\Users\TA\workshop\week16\sample\Megalovania\", "newTone.txt", "duration.txt", "newMegalovania.wav", 1);
+        }
+
+        public static void Hard_ModulateTheBPM()
+        {
+            string durations = "";
+            using (StreamReader sr = new StreamReader(@"C:\Users\TA\workshop\week16\sample\Wily'sCastle\duration.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] times = line.Split(" ");
+                    for (int i = 0; i < times.Length; i++)
+                    {
+                        if (times[i]=="") 
+                        {
+                            durations += "\n";
+                        }
+                        else
+                        {
+                            double time = Double.Parse(times[i]) / 1.5;
+                            durations += $"{time.ToString()} ";
+                        }
+                    }
+                }
+                durations = durations.Remove(durations.Length-1);
+            }
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\TA\workshop\week16\sample\Wily'sCastle\newDuration.txt"))
+            {
+                sw.Write(durations);
+            }
+            MakeMusic.Wav(@"C:\Users\TA\workshop\week16\sample\Wily'sCastle\", "tone.txt", "newDuration.txt", "newWily'sCastle.wav", 1);
+        }
+    }
+}
